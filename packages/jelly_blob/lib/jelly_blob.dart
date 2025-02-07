d83f56b3d3ec20713e7a5df6aa1e17ae37ff76d3
suojae <suojae.developer@gmail.com>
2025-02-07 21:54:56 +0900

    fix: export 파일경로 수정

--- packages/jelly_blob/lib/jelly_blob.dart
+++ packages/jelly_blob/lib/jelly_blob.dart
@@ -1,3 +1,5 @@
 export 'package:jelly_blob/blob_animation/animated_blob.dart';
+export 'package:jelly_blob/blob_animation/blob_button.dart';
+export 'package:jelly_blob/blob_animation/blob_cliper.dart';
+export 'package:jelly_blob/blob_animation/blob_path.dart';
 export 'package:jelly_blob/blob_animation/blob_painter.dart';
-export 'package:jelly_blob/blob_animation/blob_wraper.dart';
